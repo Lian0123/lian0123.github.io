b596162e4433966b4a17510353a60aa1d6e6d1f2
lian0123 <lienyungli123@gmail.com>
2020-12-04 00:02:10 +0800

    修正一個小bug

--- Source/JS/index.js
+++ Source/JS/index.js
@@ -8,9 +8,8 @@ setTimeout(() => {
     document.getElementsByTagName("div").topMask.style.display  = "none";
 }, 1000);
 
-document.getElementsByTagName("div").mainView.style.display     = "";/*
+document.getElementsByTagName("div").mainView.style.display     = "";
 document.getElementsByTagName("div").documentView.style.display = "none";
 document.getElementsByTagName("div").projectView.style.display  = "none";
 document.getElementsByTagName("div").aboutView.style.display    = "none";
 document.getElementsByTagName("div").blogView.style.display     = "none";
-*/
